ec09e57fb26ca1592d10863fdfac5e2f9ff4cea7
nwaf <eng.nwaf@gmail.com>
2023-02-09 16:00:42 +0300

    login work on

--- projects/admin/src/app/auth/components/login/login.component.ts
+++ projects/admin/src/app/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb:FormBuilder) { }
 
+  loginForm!:FormGroup;
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm(){
+    this.loginForm=this.fb.group({
+      email:['',[Validators.email,Validators.required]],
+      password:['',[Validators.required,Validators.min(3),Validators.max(20)]]
+    })
+  }
+
+  login(){
+    console.log(this.loginForm.value);
+  }
 
 }
